2eea4b2c7d856f1d9ed000cdb13c29dbf6418cf8
ToBeMaster <fist0718@gmail.com>
2022-06-13 17:23:31 +0900

    Init Commit

--- camper/src/main/java/com/camper/CamperApplication.java
+++ camper/src/main/java/com/camper/CamperApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.camper.controller, com.camper.model, com.camper.service, com.camper.mapper, com.camper.search"})
 public class CamperApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CamperApplication.class, args);
+		System.out.println("서버시작");
 	}
 
 }
